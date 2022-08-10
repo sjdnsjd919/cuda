cc2479bf674abf91e3a3fa733ffe2f3f14ebc114
sjdnsjd919 <chauhm1@kgv.hk>
2022-08-10 13:40:44 +0800

    im so ded

--- vectorAdd.py
+++ vectorAdd.py
@@ -1,22 +1,20 @@
 import numpy as np
 from timeit import default_timer as timer
-from numba import vectorize
-#6.78 s for 1e7
-#vectprize uses scalar n input 1 output only
-@vectorize(["float32(float32,float32)"],target='cuda')#1st is output tpe, then input, default is 1 instatce in GPU
-def vectorAdd(a,b):
+from numba import vectorize, cuda
+
+@vectorize(["float32(float32,float32)"],target='cuda')
+def VectorAdd(a,b):
     return a+b
 
 def main():
-    N=10000000
-    a=np.ones(N,dtype=np.float32)
-    b=np.ones(N,dtype=np.float32)
-    c=np.zeros(N,dtype=np.float32)
-    starttime = timer()
-    c= vectorAdd(a,b)
-    timetot= timer()-starttime
-    print(str(c[:5]))
-    print(str(c[-5:]))
-    print("time:" + str(timetot))
-if __name__ == '__main__':
-    main()
+    N=32000000
+    A=np.ones(N,dtype=np.float32)
+    B=np.ones(N,dtype=np.float32)
+    C=np.zeros(N,dtype=np.float32)
+
+    start =timer()
+    VectorAdd(A,B,C)
+    timee = timer()-start
+    print(timee)
+main()
+print("hui")
